7aa48ae3900ad52af7d9e0bc57c24beaa73cef0f
Yasir Choudhury <yasirgchoudhury@utexas.edu>
2021-04-30 03:19:26 -0500

    working; before trying to make autopilot fuel nice

--- Group Project/main.c
+++ Group Project/main.c
@@ -33,7 +33,7 @@ typedef struct _state State; // refers to struct as State instead of the entire 
 const double gravity = -1.6;
 
 int validateInput(char input[1]){ // returns 1 if valid Y/N input is provided by user
-    printf("Welcome to the Lunar Lander console\nWould you like to manually control the lander?\nPress Y for yes\nPress N for no\n");
+    printf("Welcome to the Lunar Lander console\nWould you like to engage autopilot?\nPress Y for yes\nPress N for no\n");
     scanf("%s", input);
     while(strcmp(input, "Y") != 0 && strcmp(input, "y") != 0 && strcmp(input, "N") != 0 && strcmp(input, "n") != 0){
         printf("Error: Invalid Input. You're an astronaut, you know better! Try again.\n");
@@ -44,11 +44,11 @@ int validateInput(char input[1]){ // returns 1 if valid Y/N input is provided by
 
 int chooseMode(char input[1]){
     if(strcmp(input, "Y") == 0 || strcmp(input, "y") == 0){
-        printf("Selected: Game Mode\n");
+        printf("Selected: Autopilot\n");
         return 1;
     }
     else if(strcmp(input, "N") == 0 || strcmp(input, "n") == 0){
-        printf("Selected: Autopilot\n");
+        printf("Selected: Game Mode\n");
         return 0;
     }
 }
@@ -82,17 +82,22 @@ double Autopilot(State s){
     double vf = -0.9;
     double Hf = 5;
 
-    if(s.velocity > 0){
+    if(s.velocity > 0){ // do nothing if velocity is positive to avoid never landing
         return 0;
     }
 
     else if(s.altitude > Hf){
         double ad = (((s.velocity * s.velocity) - (vf * vf)) / (2 * (s.altitude - Hf)));
-        return (s.mass * (ad - gravity));
+        if((s.mass * (ad - gravity)) > 45000){
+            return 45000;
+        }
+        else{
+            return s.mass * (ad - gravity);
+        }
     }
 
     else{
-        return (-s.mass * gravity * (s.velocity / vf)); // double check this one, with regards to -s.mass. Is that alowed?
+        return (-s.mass * gravity * (s.velocity / vf));
     }
 }
 
@@ -101,34 +106,24 @@ int main(){
     int autoFlag;
     int time = 0;
     double thrust = 0;
-    double accel = 0;
 
 
     FILE* output = fopen("output.csv", "wt");
 
     State s = {15000, -325, 1800, 9000}; // initial state init
-    
-    printf("Welcome to the Lunar Lander console\nWould you like to manually control the lander?\nPress Y for yes\nPress N for no\n");
-    scanf("%s", choice);
-    // passes 1 character (+ null terminator) string to check for valid Y/N input
-    while(strcmp(choice, "Y") != 0 && strcmp(choice, "y") != 0 && strcmp(choice, "N") != 0 && strcmp(choice, "n") != 0){
-        printf("Error: Invalid Input. You're an astronaut, you know better! Try again.\n");
-        scanf("%s", choice);
-    }
 
-    if (strcmp(choice, "Y") == 0 || strcmp(choice, "y") == 0){
-        autoFlag = 0;
- 
-    }
-    else if (strcmp(choice, "N") == 0 || strcmp(choice, "n") == 0){
+    validateInput(choice);
+
+    if(chooseMode(choice) == 1){
         autoFlag = 1;
+    }
+    else{
+        autoFlag = 0;
     }
 
     // display initial struct state
     printf("Altitude: %5.0lf    Velocity: %6.1lf    Mass: %6.1lf    Fuel: %.2lf\n", s.altitude, s.velocity, s.mass, s.fuel);
     fprintf(output, "%d,%.0lf,%.1lf,%.1lf\n", time++, thrust, s.altitude, (s.velocity * -100));
-
-    printf("Autoflag = %i\n", autoFlag);
 
     while(s.altitude > 0){
         int input;
@@ -159,7 +154,7 @@ int main(){
     }
 
     printf("\n");
-    if(s.velocity >= 1.1){
+    if(s.velocity >= -1 && s.velocity <= 0){
         printf("Touchdown!\n");
     }
 
