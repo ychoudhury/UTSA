a350de6dff2c9f2ba58a6e4aa5e12a3e31985a18
Yasir Choudhury <yasirgchoudhury@utexas.edu>
2021-09-22 15:47:29 -0500

    Update Memory_64byte_tb.v

--- 4513/Lab 3/Memory_64byte_tb.v
+++ 4513/Lab 3/Memory_64byte_tb.v
@@ -12,7 +12,7 @@ wire [7:0] D_OUT;
 //UUT
 Memory_64byte M1(D_IN, ADDR, R_ENABLE, W_ENABLE, RESET, CLK, D_OUT);
 
-initial begin
+initial begin // initially clear all memory locations
 D_IN = 8'b00000000;
 ADDR = 3'b000;
 R_ENABLE = 1'b0;
@@ -21,12 +21,35 @@ RESET = 1'b0;
 CLK = 1'b0;
 #10
 
-ADDR = 3'b010;
-W_ENABLE = 1'b1;
 D_IN = 8'b11011101;
+ADDR = 3'b010;
+RESET = 1'b0;
+R_ENABLE = 1'b0;
+W_ENABLE = 1'b1;
+RESET = 1'b0;
 #10
-W_ENABLE = 1'b0;
 R_ENABLE = 1'b1;
+W_ENABLE = 1'b0;
+
+#10
+D_IN = 8'b00000011;
+ADDR = 3'b011;
+R_ENABLE = 1'b0;
+W_ENABLE = 1'b1;
+RESET = 1'b0;
+#10
+R_ENABLE = 1'b1;
+W_ENABLE = 1'b0;      
+
+#10
+D_IN = 8'b11111111;
+ADDR = 3'b110;
+R_ENABLE = 1'b0;
+W_ENABLE = 1'b1;
+RESET = 1'b1;
+#10
+R_ENABLE = 1'b1;
+W_ENABLE = 1'b0;   
 
 #10;
 
