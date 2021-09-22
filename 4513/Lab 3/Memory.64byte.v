4f83e74bda3675ed08632186a2cd14350aad3e79
Yasir Choudhury <yasirgchoudhury@utexas.edu>
2021-09-22 15:47:20 -0500

    Update Memory.64byte.v

--- 4513/Lab 3/Memory.64byte.v
+++ 4513/Lab 3/Memory.64byte.v
@@ -9,22 +9,38 @@ input R_ENABLE, W_ENABLE, RESET, CLK;
 output [7:0] D_OUT;
 
 reg [7:0] mem [2:0]; // 8-bit wide and 8 deep memory block (64 Bytes)
+reg [7:0] x;
 
 integer i; // used to clear row of memory
 
 always @(posedge CLK) begin
 
     if(RESET) begin // clear memory
-        for(i = 0; i < 7; i = i + 1)
-            mem[i] = 0;
+        x <= 8'b0000000;
+        
         end
             
+    else if(R_ENABLE) begin
+        if(ADDR >= 3'b000 && ADDR <= 3'b111) begin //check if valid address is given
+                x = mem[ADDR];
+        end
+        else begin
+            x = 8'bZZZZZZZZ; // read disable will not return valid values
+        end
+    end
+    
     else if(W_ENABLE) begin
-        mem[ADDR] <= D_IN; // non-blocking statement to immediately move all input into specified address
+        if(ADDR >= 3'b000 && ADDR <= 3'b111) begin
+                x <= D_IN; // non-blocking statement to immediately move all input into specified address
+        end
+        else begin
+        // NOP
+        end
     end
-    
+    $display("Time = %0t, Data In = %8b, Address = %3b, Read Enable = %1b, Write Enable = %1b, Reset = %1b, Data Out = %8b", $time, D_IN, ADDR, R_ENABLE, W_ENABLE, RESET, D_OUT);
     end
-    
-    assign D_OUT = (R_ENABLE && ~W_ENABLE) ? mem[ADDR] : 8'bZZZZZZZZ; // output either contents of memory address or no connection
-        
+
+   assign D_OUT = x;
+   
+
 endmodule
