e8ccb5db895a307851444166a1334ee96325c787
Yasir Choudhury <yasirgchoudhury@utexas.edu>
2021-09-22 21:18:54 -0500

    Update Memory_64byte_tb.v

--- 4513/Lab 3/Memory_64byte_tb.v
+++ 4513/Lab 3/Memory_64byte_tb.v
@@ -4,55 +4,86 @@
 
 module Memory_64byte_tb;
 
-reg [7:0] D_IN;
+reg [63:0] D_IN;
 reg [2:0] ADDR;
 reg R_ENABLE, W_ENABLE, RESET, CLK;
-wire [7:0] D_OUT;
+wire [63:0] D_OUT;
 
 //UUT
 Memory_64byte M1(D_IN, ADDR, R_ENABLE, W_ENABLE, RESET, CLK, D_OUT);
 
-initial begin // initially clear all memory locations
-D_IN = 8'b00000000;
+// initially set all control values to 0
+initial begin 
+D_IN = 16'h0000000000000000;
 ADDR = 3'b000;
 R_ENABLE = 1'b0;
 W_ENABLE = 1'b0;
 RESET = 1'b0;
 CLK = 1'b0;
-#10
+end
 
-D_IN = 8'b11011101;
-ADDR = 3'b010;
-RESET = 1'b0;
+always #10 CLK = ~CLK;
+
+initial begin
+
+#10
+D_IN = 8'b00000001;
+ADDR = 3'b000;
 R_ENABLE = 1'b0;
 W_ENABLE = 1'b1;
 RESET = 1'b0;
-#10
+
+#20
+D_IN = 8'b00000001;
+ADDR = 3'b000;
 R_ENABLE = 1'b1;
 W_ENABLE = 1'b0;
+RESET = 1'b0;
 
-#10
-D_IN = 8'b00000011;
-ADDR = 3'b011;
+#20
+D_IN = 8'b00000001;
+ADDR = 3'b000;
+R_ENABLE = 1'b1;
+W_ENABLE = 1'b1;
+RESET = 1'b0;
+
+#20
+D_IN = 8'b00000001;
+ADDR = 3'b000;
+R_ENABLE = 1'b1;
+W_ENABLE = 1'b0;
+RESET = 1'b1;
+
+//
+
+#20
+D_IN = 8'b00000010;
+ADDR = 3'b000;
 R_ENABLE = 1'b0;
 W_ENABLE = 1'b1;
 RESET = 1'b0;
-#10
-R_ENABLE = 1'b1;
-W_ENABLE = 1'b0;      
 
-#10
-D_IN = 8'b11111111;
-ADDR = 3'b110;
-R_ENABLE = 1'b0;
+#20
+D_IN = 8'b00000010;
+ADDR = 3'b000;
+R_ENABLE = 1'b1;
+W_ENABLE = 1'b0;
+RESET = 1'b0;
+
+#20
+D_IN = 8'b00000010;
+ADDR = 3'b000;
+R_ENABLE = 1'b1;
 W_ENABLE = 1'b1;
-RESET = 1'b1;
-#10
-R_ENABLE = 1'b1;
-W_ENABLE = 1'b0;   
+RESET = 1'b0;
 
-#10;
+#20
+D_IN = 8'b00000010;
+ADDR = 3'b000;
+R_ENABLE = 1'b1;
+W_ENABLE = 1'b0;
+RESET = 1'b0;
 
 end
-always #10 CLK = ~CLK;
+
 endmodule
