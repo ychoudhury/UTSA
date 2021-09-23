08c589dd4eaa69042d9a1edb1a33345a19e3ad47
Yasir Choudhury <yasirgchoudhury@utexas.edu>
2021-09-22 23:50:31 -0500

    Update Memory_64byte_tb.v

--- 4513/Lab 3/Memory_64byte_tb.v
+++ 4513/Lab 3/Memory_64byte_tb.v
@@ -5,17 +5,17 @@
 module Memory_64byte_tb;
 
 reg [7:0] D_IN;
-reg [7:0] ADDR;
+reg [63:0] ADDR;
 reg R_ENABLE, W_ENABLE, RESET, CLK;
 wire [7:0] D_OUT;
 
 //UUT
 Memory_64byte M1(D_IN, ADDR, R_ENABLE, W_ENABLE, RESET, CLK, D_OUT);
 
-// initially set all control values to 0
+// initialize all control values to 0
 initial begin 
 D_IN = 8'b00000000;
-ADDR = 3'b000;
+ADDR = 'd0;
 R_ENABLE = 1'b0;
 W_ENABLE = 1'b0;
 RESET = 1'b0;
@@ -25,65 +25,86 @@ end
 always #10 CLK = ~CLK;
 
 initial begin
-
+// write 1 to address 0
+// should expect high Z output because we are not reading anything
 #10
 D_IN = 8'b00000001;
-ADDR = 3'b000;
+ADDR = 'd0;
 R_ENABLE = 1'b0;
-W_ENABLE = 1'b1;
+W_ENABLE = 1'b1; 
 RESET = 1'b0;
 
+// read output of address 0
+//should expect 1 from previous statements
 #20
 D_IN = 8'b00000001;
-ADDR = 3'b000;
+ADDR = 'd0;
 R_ENABLE = 1'b1;
 W_ENABLE = 1'b0;
 RESET = 1'b0;
 
+// enable read and write at the same time
+// read should prioritize based on module logic
 #20
 D_IN = 8'b00000001;
-ADDR = 3'b000;
+ADDR = 'd0;
 R_ENABLE = 1'b1;
 W_ENABLE = 1'b1;
 RESET = 1'b0;
 
+// enable reset
+// output should be 0
 #20
 D_IN = 8'b00000001;
-ADDR = 3'b000;
+ADDR = 'd0;
 R_ENABLE = 1'b1;
-W_ENABLE = 1'b0;
+W_ENABLE = 1'b1;
 RESET = 1'b1;
 
-//
-
+// same as previous code with binary 2 input into address dec 50
 #20
 D_IN = 8'b00000010;
-ADDR = 3'b000;
+ADDR = 'd50;
 R_ENABLE = 1'b0;
 W_ENABLE = 1'b1;
 RESET = 1'b0;
 
 #20
 D_IN = 8'b00000010;
-ADDR = 3'b000;
+ADDR = 'd50;
 R_ENABLE = 1'b1;
 W_ENABLE = 1'b0;
 RESET = 1'b0;
 
 #20
 D_IN = 8'b00000010;
-ADDR = 3'b000;
+ADDR = 'd50;
 R_ENABLE = 1'b1;
 W_ENABLE = 1'b1;
 RESET = 1'b0;
 
 #20
 D_IN = 8'b00000010;
-ADDR = 3'b000;
+ADDR = 'd0;
+R_ENABLE = 1'b1;
+W_ENABLE = 1'b1;
+RESET = 1'b1;
+ 
+// read and write to memory address outside of 64 byte range
+// should expect high-Z impedance
+#20
+D_IN = 8'b00000010;
+ADDR = 'd70;
+R_ENABLE = 1'b1;
+W_ENABLE = 1'b1;
+RESET = 1'b0;
+
+#20
+D_IN = 8'b00000010;
+ADDR = 'd70;
 R_ENABLE = 1'b1;
 W_ENABLE = 1'b0;
 RESET = 1'b0;
-
 
 
 end
