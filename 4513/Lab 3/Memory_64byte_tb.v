c62bd7d82a7233f5e5692f12bc948d1d96e6214b
Yasir Choudhury <yasirgchoudhury@utexas.edu>
2021-09-22 22:26:23 -0500

    Update Memory_64byte_tb.v

--- 4513/Lab 3/Memory_64byte_tb.v
+++ 4513/Lab 3/Memory_64byte_tb.v
@@ -4,17 +4,17 @@
 
 module Memory_64byte_tb;
 
-reg [63:0] D_IN;
-reg [2:0] ADDR;
+reg [7:0] D_IN;
+reg [7:0] ADDR;
 reg R_ENABLE, W_ENABLE, RESET, CLK;
-wire [63:0] D_OUT;
+wire [7:0] D_OUT;
 
 //UUT
 Memory_64byte M1(D_IN, ADDR, R_ENABLE, W_ENABLE, RESET, CLK, D_OUT);
 
 // initially set all control values to 0
 initial begin 
-D_IN = 16'h0000000000000000;
+D_IN = 8'b00000000;
 ADDR = 3'b000;
 R_ENABLE = 1'b0;
 W_ENABLE = 1'b0;
@@ -83,6 +83,8 @@ ADDR = 3'b000;
 R_ENABLE = 1'b1;
 W_ENABLE = 1'b0;
 RESET = 1'b0;
+
+
 
 end
 
