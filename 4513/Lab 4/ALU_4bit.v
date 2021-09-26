9d0ce9f95abc24fd61b627acb1a82171c5439b22
Yasir Choudhury <yasirgchoudhury@utexas.edu>
2021-09-25 20:35:47 -0500

    Implemented adder

--- 4513/Lab 4/ALU_4bit.v
+++ 4513/Lab 4/ALU_4bit.v
@@ -9,21 +9,28 @@ output wire Cout;
 reg [3:0] r;
 reg c;
 
-
-
 always @(*) begin // * used for combinational logic
 
 case(Control)
-    3'b000: 
-        {r, c} = A + B + Cin;
+    3'b000: // Instruction: Add
+        {c, r[3:0]} <= (A[3:0] + B[3:0] + Cin);
+    3'b001: // Instruction: Subtract
+        r = 1'b1;
         
-    3'b001: r = 1'b1;
-    3'b010: r = 1'b1;
-    3'b011: r = 1'b1;
-    3'b100: r = 1'b1;
-    3'b101: r = 1'b1;
-    3'b110: r = 1'b1;
-    3'b111: r = 1'b1;
+    3'b010: // Instruction: OR
+        r = (A | B);
+        
+    3'b011: // Instruction: AND
+        r = (A & B);
+        
+    3'b100: // Instruction: Shift Left
+        r = 1'b1;
+    3'b101: // Instruction: Shift Right
+        r = 1'b1;
+    3'b110: // Instruction: ROL
+        r = 1'b1;
+    3'b111: // Instruction: ROR
+        r = 1'b1;
 
 endcase
 end
