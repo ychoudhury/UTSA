4336df63fe18e6c0b4083d29f4b56439789b1268
Yasir Choudhury <yasirgchoudhury@utexas.edu>
2021-11-18 14:54:25 -0600

    Update SPFPM.v

--- 4513/Lab 9/SPFPM.v
+++ 4513/Lab 9/SPFPM.v
@@ -6,7 +6,7 @@
 module multiplier(A, B, reset, out);
 
 input [31:0] A, B;
-input reset = 1'b0;
+input reset;
 output [31:0] out;
 
 // define components of Single Precision Floating Point
@@ -22,40 +22,38 @@ reg out_sign;
 reg [7:0] out_exponent;
 reg [23:0] out_mantissa;
 
-
 reg isNormal = 1'b0;
 reg [47:0] mantissaMultiplicand;
-reg [22:0] mantissaNormalised = 23'b0;
+reg [22:0] mantissaNormalised;
 
-reg [31:0] outputReg = 32'b0;
+reg [31:0] product;
 integer i;
 
 
 always @(*) begin
 
     if(reset == 1) begin // reset flag
-        outputReg <= 32'b00000000000000000000000000000000;
+        product <= 32'b00000000000000000000000000000000;
     end
     
     else if(A == 0 || B == 0) begin // early out
-        outputReg <= 32'b00000000000000000000000000000000;
+        product <= 32'b00000000000000000000000000000000;
     end
 
     else begin
-        A_mantissa = {1'b1, A[22:0]};
-        B_mantissa = {1'b1, B[22:0]}; 
-    
-        mantissaNormalised = 23'b0;
-        
-        A_exponent = A[30:23];
-        B_exponent = B[30:23];
-        
         A_sign = A[31];
+        A_exponent = A[30:23];
+        A_mantissa = {1'b1, A[22:0]};
+    
+        
         B_sign = B[31];
+        B_exponent = B[30:23];
+        B_mantissa = {1'b1, B[22:0]}; 
         
-        isNormal = 1'b0;
-        
+        isNormal = 1'b0; // flag
+        mantissaNormalised = 23'b0;
         mantissaMultiplicand = (A_mantissa * B_mantissa);
+        product = 32'b0;
                 
         if(mantissaMultiplicand & (1 << 47)) begin      
             out_exponent = (A_exponent + B_exponent - 126);        
@@ -64,7 +62,6 @@ always @(*) begin
         else begin
             out_exponent = (A_exponent + B_exponent - 127);
         end
-                        // up until here we're good
 
         for(i = 47; i > 0; i = i - 1) begin
             if(isNormal == 1'b0) begin
@@ -77,13 +74,13 @@ always @(*) begin
         
         out_sign = A_sign ^ B_sign;
         
-        outputReg = {out_sign, out_exponent, mantissaNormalised};
+        product = {out_sign, out_exponent, mantissaNormalised};
         
     end
     
 end
                 
-assign out = outputReg;
+assign out = product;
     
         
 endmodule
