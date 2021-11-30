71fc4e369c4dd6265c8bd7337efb7d6683dcd2d5
Yasir Choudhury <yasirgchoudhury@utexas.edu>
2021-11-30 10:49:28 -0600

    prettifying 2

--- 4513/FPU/Adder/add.v
+++ 4513/FPU/Adder/add.v
@@ -4,13 +4,13 @@ input wire clk;
 input wire rst;
 input wire start;
 
-  input     [31:0] input_a;
-  input     input_a_stb;
-  output    input_a_ack;
+input wire [31:0] input_a;
+input input_a_stb;
+output input_a_ack;
 
-  input     [31:0] input_b;
-  input     input_b_stb;
-  output    input_b_ack;
+input wire [31:0] input_b;
+input input_b_stb;
+output input_b_ack;
 
   output    [31:0] output_z;
   output    output_z_stb;
