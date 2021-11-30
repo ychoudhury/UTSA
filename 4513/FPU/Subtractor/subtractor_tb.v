9ad43a11006e6e588dce25dfbdf75c3b5b2188d2
Yasir Choudhury <yasirgchoudhury@utexas.edu>
2021-11-30 13:10:39 -0600

    works

--- 4513/FPU/Subtractor/subtractor_tb.v
+++ 4513/FPU/Subtractor/subtractor_tb.v
@@ -5,54 +5,33 @@ module subtractor_tb;
 reg [31:0] input_a;
 reg [31:0] input_b;
 wire [31:0] output_z;
-reg clk, rst;
-reg input_a_stb, input_b_stb, output_z_ack;
+reg clk;
+reg rst;
+reg start;
+reg ack_output;
+wire idle_status;
+wire output_valid;
 
 //UUT
-subtractor S1(input_a,
-        input_b,
-        input_a_stb,
-        input_b_stb,
-        output_z_ack,
-        clk,
-        rst,
-        output_z,
-        output_z_stb,
-        input_a_ack,
-        input_b_ack
-);
+subtractor S1(input_a, input_b, start, ack_output, clk, rst, output_z, output_valid, idle_status);
         
 initial begin
 input_a <= 32'b0;
 input_b <= 32'b0;
 clk <= 1'b0;
 rst <= 1'b0;
-input_a_stb <= 1'b1;
-input_b_stb <= 1'b1;
-output_z_ack <= 1'b1;
+ack_output <= 1'b1;
+start <= 1'd1;
 end
          
 always #10 clk = ~clk;
 
 initial begin
-//#10
-//input_a <= 32'b01000001011111000000000000000000; // 15.75
-//input_b <= 32'b01000000111010000000000000000000; // 7.25
-//rst <= 0;
 
-//#10
-//input_a <= 32'b01000001011111000000000000000000; // 15.75
-//input_b <= 32'b01000000111010000000000000000000; // 7.25
-//rst <= 1;
-
-//#10
-//input_a <= 32'b01000001011111000000000000000000; // 15.75
-//input_b <= 32'b01000000111010000000000000000000; // 7.25
-//rst <= 0;
-
-#10
-input_a <= 32'b00000000000000000000000000000000; // 0
+#50
+input_a <= 32'b01000011001111101001010111000011; // 190.585
 input_b <= 32'b01000000111010000000000000000000; // 7.25
 rst <= 0;
 end
+
 endmodule
