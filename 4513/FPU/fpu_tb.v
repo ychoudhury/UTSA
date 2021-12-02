e2c8788cc91ac01f21f446ef757060519d076d35
Yasir Choudhury <yasirgchoudhury@utexas.edu>
2021-12-01 20:13:41 -0600

    Working code from Mohammed

--- 4513/FPU/fpu_tb.v
+++ 4513/FPU/fpu_tb.v
@@ -1,10 +1,7 @@
 `timescale 1ns / 1ps
 
-
-module fpu_alu_tb(
-
-    );
-    
+module fpu_tb;
+   
     reg clk;
     reg rst;
     reg start;
@@ -60,16 +57,31 @@ module fpu_alu_tb(
         input_b = 32'd0;
         
         //VALUES FOR OP 0 (IF YOUR OPERATION NEEDS TWO INPUTS, ALSO ADD IN arrValsB)
-        arrValsA[0] = 32'h3fc00000; //1.5
-        arrValsA[1] = 32'h41500000; //13
+        arrValsA[0] = 32'b01000011001111101001010111000011; // 190.585
+        arrValsB[0] = 32'b01000000111010000000000000000000; // 7.25
+        
+        arrValsA[1] = 32'h40200000; //2.5
+        arrValsB[1] = 32'h40200000; //2.5
+        
         arrValsA[2] = 32'hc348999a; //-200.6
+        arrValsB[2] = 32'h00000000; // 0
+        
         arrValsA[3] = 32'h00000000; // 0
+        arrValsB[3] = 32'h00000000; // 0
+
         
         //VALUES FOR OP 1
-        arrValsA[4] = 32'h00000002; //2
-        arrValsA[5] = 32'hFFFFFFF4; //-12
-        arrValsA[6] = 32'h00000000; // 0
-        arrValsA[7] = 32'h0007D214; // 512532
+        arrValsA[4] = 32'b01000011001111101001010111000011; // 190.585
+        arrValsB[4] = 32'b01000000111010000000000000000000; // 7.25
+        
+        arrValsA[5] = 32'h40200000; //2.5
+        arrValsB[5] = 32'h40200000; //2.5
+        
+        arrValsA[6] = 32'h42480000; // 50.0
+        arrValsB[6] = 32'h4159999A; // 13.6
+        
+        arrValsA[7] = 32'hc348999a; // -200.6
+        arrValsB[7] = 32'h4159999A; // 512532
         
         //VALUES FOR OP 2
         arrValsA[8] = 32'h00000002; //2
@@ -78,22 +90,36 @@ module fpu_alu_tb(
         arrValsA[11] = 32'h0007D214; // 512532
         
         //VALUES FOR OP 3
-        arrValsA[12] = 32'h00000002; //2
-        arrValsA[13] = 32'hFFFFFFF4; //-12
-        arrValsA[14] = 32'h00000000; // 0
-        arrValsA[15] = 32'h0007D214; // 512532
+        arrValsA[12] = 32'h3fc00000; //1.5
+        arrValsA[13] = 32'h41500000; //13
+        arrValsA[14] = 32'hc348999a; //-200.6
+        arrValsA[15] = 32'h00000000; // 0
         
         //VALUES FOR OP 4
-        arrValsA[16] = 32'h00000002; //2
-        arrValsA[17] = 32'hFFFFFFF4; //-12
-        arrValsA[18] = 32'h00000000; // 0
-        arrValsA[19] = 32'h0007D214; // 512532
+        arrValsA[16] = 32'b01000011001111101001010111000011; // 190.585
+        arrValsB[16] = 32'b01000000111010000000000000000000; // 7.25
+        
+        arrValsA[17] = 32'h40200000; //2.5
+        arrValsB[17] = 32'h40200000; //2.5
+        
+        arrValsA[18] = 32'hc348999a; //-200.6
+        arrValsB[18] = 32'h00000000; // 0
+        
+        arrValsA[19] = 32'h00000000; // 0
+        arrValsB[19] = 32'h00000000; // 0
         
         //VALUES FOR OP 5
-        arrValsA[20] = 32'h00000002; //2
-        arrValsA[21] = 32'hFFFFFFF4; //-12
-        arrValsA[22] = 32'h00000000; // 0
-        arrValsA[23] = 32'h0007D214; // 512532
+        arrValsA[20] = 32'h449a4000; //2.5
+        arrValsB[20] = 32'hff4d1150; //2.5
+        
+        arrValsA[21] = 32'b01000011001111101001010111000011; // 190.585
+        arrValsB[21] = 32'b01000000111010000000000000000000; // 7.25
+        
+        arrValsA[22] = 32'h40800000; //4.0
+        arrValsB[22] = 32'h40000000; //2.0
+        
+        arrValsA[23] = 32'h41700000; //15.0
+        arrValsB[23] = 32'h40400000; //3.0
         
         valWrittenFlag = 1;
     end
@@ -118,7 +144,7 @@ module fpu_alu_tb(
                 i_signed = i_signed + 1;
             end
             input_a = arrValsA[i];
-            input_b = arrValsA[i];
+            input_b = arrValsB[i];
             #15;
             start = 1'b1;
             while(o_valid != 1'b1) begin
@@ -128,3 +154,5 @@ module fpu_alu_tb(
         end
     end
 endmodule
+
+
